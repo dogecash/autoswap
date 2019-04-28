2d767490c4146f13815ce1aa103443960e06a14c
Akshay <akshaycm@hotmail.com>
2019-04-28 14:59:48 +0300

    update to give out more info

--- blacklisttotal.js
+++ blacklisttotal.js
@@ -25,6 +25,7 @@ request(apiurl + 'api/alladdrs', { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
     addrdata = body;
     var i,j;
+    var totalsupply=0;
     var badaddrs = [ "DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8",  "DT9LxyfGn91gAWhXedSf81B7ATLseSxuVv",
     "DJM1uEdrCiSzZRk9hwpaFi1DmYNFh2gpxL", "DBHP5rx1dyhgyo6Chpt4mqe5ZXYBc7zpHb",
     "DRaaCkzhk9zM76rwcgBmgf5UfemS7bCRBC", "DAYyhPf9iijgjWU9nf52BveccLdgWp5DLw",
@@ -32,14 +33,19 @@ request(apiurl + 'api/alladdrs', { json: true }, (err, res, body) => {
     var totalexploitedamt = 0;
    // console.log(addrdata.length);
     // if(addrdata != null){
+console.log("Calculating blacklisted total amount... Please wait")
 for(i=0;i < addrdata.length;i++){
-   
+    totalsupply = totalsupply + addrdata[i].value;
    for (j=0;j < badaddrs.length;j++){
     if(addrdata[i].address == (badaddrs[j])){
         totalexploitedamt= totalexploitedamt + addrdata[i].value;
     }
+  
    }
 }
+console.log("Caclulation Complete")
+console.log("Total Addresses on Chain: " +addrdata.length);
+console.log("Total Supply: " +totalsupply);
 console.log("Total Blacklisted DOGEC : " +totalexploitedamt);
 //console.log(totalexploitedamt);
    // }
