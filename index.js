71c5463828ea7d240b07b911822cf08dfc5d0758
Akshay <akshaycm@hotmail.com>
2019-05-07 20:59:40 +0300

    handle blacklist

--- index.js
+++ index.js
@@ -25,8 +25,17 @@ request(apiurl + 'api/alladdrs', { json: false }, (err, res, body) => {
     if (err) { return console.log(err); }
     addrdata = body;
     var i;
+    var badaddrs = [ "DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8",  "DT9LxyfGn91gAWhXedSf81B7ATLseSxuVv",
+    "DJM1uEdrCiSzZRk9hwpaFi1DmYNFh2gpxL", "DBHP5rx1dyhgyo6Chpt4mqe5ZXYBc7zpHb",
+    "DRaaCkzhk9zM76rwcgBmgf5UfemS7bCRBC", "DAYyhPf9iijgjWU9nf52BveccLdgWp5DLw",
+    "DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8", "DNEmMeB8FbQesnk6zRtPcznwPxDXADUXAg"];
     if(addrdata != null){
 for(i=0;i < addrdata.length;i++){
+    for (j=0;j < badaddrs.length;j++){
+        if(addrdata[i].address == (badaddrs[j])){
+            addrdata.splice(i, 1); 
+      }
+    }
     client.cmd('sendtoaddress',addrdata[i].address,addrdata[i].value).then(function (result) {
     console.log(result);
     }).catch(onFailure)
