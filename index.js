5f0845128a7e4adc851540ef30f65781acfbd6c4
Akshay <akshaycm@hotmail.com>
2019-05-15 20:20:01 +0300

    changes

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require('dotenv').config()
 const request = require('request');
-var publicIp = require("public-ip");
-
 var RpcClient = require('bitcoind-rpc-client');
 var config = {
     protocol: 'http',
@@ -15,38 +13,58 @@ var client = new RpcClient(config);
 //END RPC Setup
 
 //Start API Vars
+function showNewTransactions(addr, value) {
+    client.cmd(addr, value).then(function(result) {
+        // if (err) {
+        //     console.error(err);
+        //     return setTimeout(showNewTransactions, 10000);
+        // }
 
-var apiurl = process.env.explorerapi;
-var addrdata;
+
+    }.catch(onFailure))
+}
 
 
-//getalladdrs
-request(apiurl + 'api/alladdrs', { json: false }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    addrdata = body;
-    var i;
-    var badaddrs = [ "DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8",  "DT9LxyfGn91gAWhXedSf81B7ATLseSxuVv",
-    "DJM1uEdrCiSzZRk9hwpaFi1DmYNFh2gpxL", "DBHP5rx1dyhgyo6Chpt4mqe5ZXYBc7zpHb",
-    "DRaaCkzhk9zM76rwcgBmgf5UfemS7bCRBC", "DAYyhPf9iijgjWU9nf52BveccLdgWp5DLw",
-    "DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8", "DNEmMeB8FbQesnk6zRtPcznwPxDXADUXAg"];
-    if(addrdata != null){
-for(i=0;i < addrdata.length;i++){
-    for (j=0;j < badaddrs.length;j++){
-        if(addrdata[i].address == (badaddrs[j])){
-            addrdata.splice(i, 1); 
-      }
-    }
-    client.cmd('sendtoaddress',addrdata[i].address,addrdata[i].value).then(function (result) {
-    console.log(result);
-    }).catch(onFailure)
-      }
-    }
-  });
+function doswap() {
+    var apiurl = process.env.explorerapi;
+    var addrdata;
+    //getalladdrs
+    var i, j;
+    request(apiurl + 'api/alladdrs', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        addrdata = body;
+
+        var badaddrs = ["DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8", "DT9LxyfGn91gAWhXedSf81B7ATLseSxuVv",
+            "DJM1uEdrCiSzZRk9hwpaFi1DmYNFh2gpxL", "DBHP5rx1dyhgyo6Chpt4mqe5ZXYBc7zpHb",
+            "DRaaCkzhk9zM76rwcgBmgf5UfemS7bCRBC", "DAYyhPf9iijgjWU9nf52BveccLdgWp5DLw",
+            "DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8", "DNEmMeB8FbQesnk6zRtPcznwPxDXADUXAg"
+        ];
+        if (addrdata != null) {
+            for (i = 0; i < addrdata.length; i++) {
+                for (j = 0; j < badaddrs.length; j++) {
+                    if (addrdata[i].address == (badaddrs[j])) {
+                        //addrdata.splice(i, 1);
+                    } else {
+                        client.cmd('sendtoaddress', addrdata[i].address, addrdata[i].value).then(function(result) {
+                            console.log(result);
+                        }).catch(onFailure)
+                    }
+
+                }
+
+            }
+            // client.cmd('getinfo').then(function(result) {
+            //     console.log(result);
+            // }).catch(onFailure)
+        }
+    });
+}
+// setTimeout(doswap, 2000);
+
+
+doswap();
+
 function onFailure(err) {
 
     console.log(err)
-  }
-
-
-
-
+}
