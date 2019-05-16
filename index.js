05b125bae265630b4cec60f4e3e74a5345d0d767
JSKitty <loopylew113@protonmail.com>
2019-05-16 17:05:05 +0100

    Add bulk-queue system & optimize
    Adds a bulk-send ability into the Queue system, 25 TX'es per-round (Every 5 secs) by default, also optimized the queue processing with .shift() instead.

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 require('dotenv').config()
-const request = require('request');
-var RpcClient = require('bitcoind-rpc-client');
+const request = require('request')
+var RpcClient = require('bitcoind-rpc-client')
 var config = {
     protocol: 'http',
     user: process.env.rpcuser,
     pass: process.env.rpcpassword,
     host: '127.0.0.1',
     port: process.env.rpcport
-};
-var client = new RpcClient(config);
+}
+var client = new RpcClient(config)
 //Set RPC connection stuff
 //END RPC Setup
-var txcount =0;
 //Start API Vars
 
-var badaddrs = ["DAekFXfYfJxesti27B9dyiNwB1G7BZeiX1","DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8", "DT9LxyfGn91gAWhXedSf81B7ATLseSxuVv",
-"DJM1uEdrCiSzZRk9hwpaFi1DmYNFh2gpxL", "DBHP5rx1dyhgyo6Chpt4mqe5ZXYBc7zpHb",
-"DRaaCkzhk9zM76rwcgBmgf5UfemS7bCRBC", "DAYyhPf9iijgjWU9nf52BveccLdgWp5DLw",
-"DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8", "DNEmMeB8FbQesnk6zRtPcznwPxDXADUXAg"
-];
+var badaddrs = [
+  "DAekFXfYfJxesti27B9dyiNwB1G7BZeiX1","DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8",
+  "DT9LxyfGn91gAWhXedSf81B7ATLseSxuVv","DJM1uEdrCiSzZRk9hwpaFi1DmYNFh2gpxL",
+  "DBHP5rx1dyhgyo6Chpt4mqe5ZXYBc7zpHb","DRaaCkzhk9zM76rwcgBmgf5UfemS7bCRBC",
+  "DAYyhPf9iijgjWU9nf52BveccLdgWp5DLw","DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8",
+  "DNEmMeB8FbQesnk6zRtPcznwPxDXADUXAg"
+]
+
 /* Variables for the TX Queue */
 var txQueue = []
 
+/* The amount of TX'es to send in bulk per-queue-round */
+var bulkAmt = 25
 /*
   A TX in the txQueue is formed like this:
 
@@ -33,26 +37,45 @@ var txQueue = []
 
 /* Check for queued TX'es at a regular interval, to prevent overloading the wallet */
 function checkTxQueue () {
-  if (txQueue.length > 0 && txcount < txQueue.length) { // Don't check the queue if it's empty
+  if (txQueue.length > 0 && txQueue[0]) { // Don't check the queue if it's empty
     console.log("TX Queue: Sending TX " + txQueue.length)
-    
-    if (!txQueue[txcount].addr || !txQueue[txcount].amt) return // Make sure a TX isn't malformed
-    console.log("Sending to adddr :" + txQueue[txcount].addr +" Val:" + txQueue[txcount].amt);
-    txcount = txcount + 1;
-    console.log(txcount + "TXCOUNT")
 
-    client.cmd('sendtoaddress', txQueue[txcount].addr, txQueue[txcount].amt).then(function(result) {
-      console.log(result)
+    if (!txQueue[0].addr || !txQueue[0].amt) return // Make sure a TX isn't malformed
+    console.log("Sending to Addr:" + txQueue[0].addr +", Val:" + txQueue[0].amt)
+
+    var i, len = bulkAmt, rawTx = '{' // Open the JSON object of TX'es
+    if (txQueue[bulkAmt]) {
+      console.log("Over " + bulkAmt + " TX'es in the queue, sending " + bulkAmt + " in bulk")
+      for (i=0; i<len; i++) { // Add each TX into the raw sendmany command
+        if (txQueue[0]) {
+          rawTx += formTx(txQueue[0].addr, txQueue[0].amt, len)
+          txQueue.shift() // Flush TX from the queue after loading raw TX into sendmany command
+        } // Don't exit, it's safe to continue
+      }
+    } else {
+      console.log("Less than " + bulkAmt + " TX'es in the queue, sending " + txQueue.length + " in bulk")
+      len = txQueue.length
+      for (i=0; i<len; i++) { // Add each TX into the raw sendmany command
+        if (txQueue[0]) {
+          rawTx += formTx(txQueue[0].addr, txQueue[0].amt, len)
+          txQueue.shift() // Flush TX from the queue after loading raw TX into sendmany command
+        } // Don't exit, it's safe to continue
+      }
+    }
+
+    rawTx += '}' // Close the JSON object of TX'es
+
+    client.cmd('sendmany', "\"\"" /* <-- Replace with account if needed */, JSON.parse(rawTx)).then(function(result) {
+      console.log("Sendmany complete: \n" + result)
     }).catch(onFailure)
+  } else {
+    console.log("Swap complete, exiting...")
+    process.exit(0)
   }
- else{
-   process.exit(0)
- }
 }
-process.on('exit', function(code) {  
-  return console.log(`Done with code ${code}\n Total TX tried :${txcount} \n ` );
-});
-/* Start the queue interval, 10 second interval */
+process.on('exit', function(code) {
+  return console.log(`Done with code ${code}`)
+})
 
 /* Validate an address, if it's good it gets pushed into the Queue, else it gets ignored */
 function validateAddr (tx) {
@@ -60,33 +83,34 @@ function validateAddr (tx) {
 	txQueue.push(tx)
 }
 
+/* Form a raw sendmany transaction using the Addr and Amt */
+function formTx (addr, amt, len) {
+  return '"' + addr + '": ' + amt + ((len < 24) ? ',' : '')
+}
+
 function doswap() {
-    var apiurl = process.env.explorerapi;
-    var addrdata;
+    var apiurl = process.env.explorerapi
     //getalladdrs
-    var i;
-    request(apiurl + 'api/alladdrs', { json: true }, (err, res, body) => {
-        if (err) { return console.log(err); }
-        addrdata = body;
+    var i, addrdata, len = addrdata.length
+    request(apiurl + 'api/alladdrs', {json: true}, (err, res, body) => {
+        if (err) return console.log(err)
+        addrdata = body
         if (addrdata != null) {
-            for (i = 0; i < addrdata.length; i++) {
-              console.log("TX Queue: Pushing new TX into queue")
+            for (i=0; i<len; i++) {
                 validateAddr({
                   addr: addrdata[i].address,
                   amt: addrdata[i].value
                 })
             }
-            setInterval(checkTxQueue, 110)
-
+            setInterval(checkTxQueue, 5000) // {bulkAmt} TX'es every 5 seconds
         }
-    });
+    })
 }
-// setTimeout(doswap, 2000);
+// setTimeout(doswap, 2000)
 
 
-doswap();
+doswap()
 
 function onFailure(err) {
-
-    console.error(err)
+  console.error(err)
 }
